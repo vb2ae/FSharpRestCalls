460753f6c02da4757b6a24abf09e455004b9ba84
Ken Tucker <vb2ae@live.com>
2021-02-21 16:19:30 -0500

    Add project files.

---
+++ FSharpRestCalls/ISS_Location.fs
@@ -0,0 +1,19 @@
+﻿module ISS_Location
+
+
+[<CLIMutable>]
+type location =
+    {
+        longitude : string
+        latitude : string
+    }
+
+[<CLIMutable>]
+type ISSLocation =
+    {
+        timestamp : int
+        message : string
+        iss_position : location
+    }
+
+
---
+++ FSharpRestCalls/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Net.Http
+open FSharp.Json
+open ISS_Location
+
+ let getAsync (client:HttpClient) (url:string) = 
+      async {
+            let! response = client.GetStringAsync(url) |> Async.AwaitTask
+            return response
+       }
+
+ let getISSLocation =
+     async {
+         let url = "http://api.open-notify.org/iss-now.json"
+         use httpClient = new System.Net.Http.HttpClient()
+         let! location = 
+             getAsync httpClient url
+         let data = Json.deserialize<ISSLocation> location
+         printfn "Returned Locatiom: %s" location
+      }
+
+
+[<EntryPoint>]
+let main argv =
+    getISSLocation
+    |> Async.RunSynchronously
+    0
+
